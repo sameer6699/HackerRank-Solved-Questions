dc2035cfa5b37711b26be6cbde93ee9d4fa4c5a6
Sameer Prashant Jadhav <sammyjadhav6699@gmail.com>
2024-07-18 15:44:06 +0530

    18-July-2024

--- Project_Eluar#9/problem_9.py
+++ Project_Eluar#9/problem_9.py
@@ -1,3 +1,4 @@
+#project Eluar 
 def max_pythagorean_triplet_product(n):
     max_product = -1
     for a in range(1, n // 3):
@@ -13,3 +14,4 @@ t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
     print(max_pythagorean_triplet_product(n))
+    
